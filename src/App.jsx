8368cda9d6f030f1a51777607afddda643e38506
sachith <kasasachith@gmail.com>
2025-06-03 16:30:45 +0530

    project started

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <h1>Hello Coder</h1>  
         <h1>Hello Coder</h1>  
       </div>
      
